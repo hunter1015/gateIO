091f02e75fa512d11372dd4ef9b48a698743f80c
hunter1015 <hunter1015@users.noreply.github.com>
2018-02-04 01:29:11 +0800

    无实质操作

--- gateAPI.py
+++ gateAPI.py
@@ -73,7 +73,7 @@ class GateIO:
             timeperiod='900'
         if timetotal==None:
             timetotal='24'
-        #URL = "/json_svr/query/?u=10&c=380225&type=kline&symbol=eth_usdt&group_sec=900&range_hour=24"
+        #URL = "https://gate.io/json_svr/query/?u=10&c=380225&type=kline&symbol=eth_usdt&group_sec=900&range_hour=24"
         #URL = "https://gate.io/json_svr/query/?u=10&c=380225&type=kline&symbol="+pairs+"&group_sec="+timeperiod+"&range_hour=24"
         URL = "/json_svr/query/?u=10&c=380225&type=kline&symbol="+pairs+"&group_sec="+timeperiod+"&range_hour="+timetotal
         #print('self.__url='+self.__url)
